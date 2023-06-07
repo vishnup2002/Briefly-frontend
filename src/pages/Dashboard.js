a0326909c79c7dcff16c74c6d0cd34d2e583a704
Vishnu P <vishnu.pothiadath@datanchor.io>
2023-06-07 21:22:37 +0530

    integrate

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,43 +1,64 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Dashboard() {
+  // const meetings = [
+  //   {
+  //     meetingName: "executive committee meeting",
+  //     date: "21/02/23",
+  //     id: "001"
+  //   },
+  //   {
+  //     meetingName: "branch head meeting",
+  //     date: "01/12/22",
+  //     id: "002"
+  //   },
+  //   {
+  //     meetingName: "zonal head meeting",
+  //     date: "30/05/23",
+  //     id: "003"
+  //   }
+  // ]
 
-  const meetings = [
-    {
-      meetingName: "executive committee meeting",
-      date: "21/02/23",
-      id: "001"
-    },
-    {
-      meetingName: "branch head meeting",
-      date: "01/12/22",
-      id: "002"
-    },
-    {
-      meetingName: "zonal head meeting",
-      date: "30/05/23",
-      id: "003"
-    }
-  ]
+  const [meetings, setMeetings] = useState([]);
 
+  useEffect(() => {
+    fetch("http://localhost:8000/dashboard", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setMeetings(data);
+      });
+  }, []);
 
   return (
     <div className="container">
       <header className="d-flex flex-wrap justify-content-between py-3 mb-4 border-bottom">
-          <span className="fs-1 ps-5">Your Meetings</span>
-          <span className="fs-1 pe-5">Date</span>
+        <span className="fs-1 ps-5">Your Meetings</span>
+        <span className="fs-1 pe-5">Date</span>
       </header>
 
-    <div className="container">
-      {meetings.map((meetings, index) => (
-        <div key={index} className='d-flex flex-wrap justify-content-between ps-2 pe-2 mb-4 border-bottom'>
-            <Link to = {`/meetings/${meetings.id}`} className="ps-5 fs-5  px-2 text-dark">{meetings.meetingName}</Link>
-          <span className="pe-5 fs-5">{meetings.date}</span>
-        </div>
-      ))}
+      <div className="container">
+        {meetings.map((meetings, index) => (
+          <div
+            key={index}
+            className="d-flex flex-wrap justify-content-between ps-2 pe-2 mb-4 border-bottom"
+          >
+            <Link
+              to={`/meetings/${meetings.id}`}
+              className="ps-5 fs-5  px-2 text-dark"
+            >
+              {meetings.meetingName}
+            </Link>
+            <span className="pe-5 fs-5">{meetings.date}</span>
+          </div>
+        ))}
+      </div>
     </div>
-
-    </div>
-  )
+  );
 }
--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,62 +1,145 @@
-import React, { useState } from 'react'
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 export default function Details() {
+  const [visible, setVisible] = useState(false);
+  const [msummary, setMsummary] = useState("Original meeting summary");
+  const navigate = useNavigate();
 
-    const [visible,setVisible] = useState(false)
-    const [msummary,setMsummary] = useState('Original meeting summary')
+  function handleDelete() {
+    console.log("delete the meeting and goto dashboard");
+    fetch(`http://localhost:8000/dashboard/${id}/edit`, {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+      });
 
-    function handleDelete(){
-        console.log('delete the meeting and goto dashboard')
-    }
+    navigate("/dashboard");
+  }
 
-    function editMeeting(){
-        console.log('Edit the meeting')
-        setVisible(true)
-    }
+  function editMeeting() {
+    console.log("Edit the meeting");
+    setVisible(true);
+  }
 
-    function saveDetails(){
-        console.log('post the meeting here')
-        console.log(msummary)
-        setVisible(false)
-    }
+  function saveDetails() {
+    console.log("post the meeting here");
+    console.log(msummary);
+    fetch(`http://localhost:8000/dashboard/${id}/edit`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+      body: JSON.stringify({ data: msummary }),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setMsummary(msummary);
+      });
 
-    const { id } = useParams();
-    console.log('details of meeting', id)
-    return (
-        <div>
+    setVisible(false);
+  }
 
-            <div className="bg-light text-secondary container shadow shadow-lg rounded px-4 py-5 text-center mt-5">
-                <div className="py-4">
-                    <h1 className="display-5 fw-bold text-dark">Meeting Name</h1>
-                    <p className='fs-5 mb-4 pe-5 d-flex justify-content-end'>Date: 27/05/2023</p>
-                    <h4 className='ps-5'>"Meeting Desc": Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima, itaque? Consequatur ad quasi ipsam exercitationem fuga facilis harum autem cupiditate! Architecto earum soluta ducimus dolore quam, aliquid veritatis possimus facere.</h4>
-                    <hr />
-                    <div className="col-lg-6 mx-auto">
-                        <p className="fs-5 mb-4">"Meeting Summary: "Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
-                        <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                            <button type="button" className="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold" onClick={editMeeting}>Edit</button>
-                            <button type="button" className="btn btn-outline-danger btn-lg px-4 " onClick={handleDelete}>Delete</button>
-                        </div>
-                    </div>
-                    <div>
+  const { id } = useParams();
+  console.log("details of meeting", id);
 
-                        {console.log(visible)}
-                        {visible ? <div className='p-3'>
-                            <div className="input-group">
-                                <span className="input-group-text">Summary</span>
-                                <textarea className="form-control focus-ring focus-ring-light" aria-label="With textarea" defaultValue='Original meeting summary' onChange={e => setMsummary(e.target.value)}></textarea>
-                            </div>
-                            <div className='d-grid gap-2 d-sm-flex justify-content-sm-center'>
-                                <button type="button" className="btn btn-sm btn-outline-success btn-lg px-4 mt-3 "  onClick={saveDetails}>Save</button>
-                                <button type="button" className="btn btn-sm btn-outline-danger btn-lg px-4 mt-3"  onClick={()=>setVisible(false)}>Cancel</button>
-                            </div>
-                        </div>:null}
-                    </div>
-                </div>
+  useEffect(() => {
+    fetch(`http://localhost:8000/dashboard/${id}`, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setMsummary(data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <div className="bg-light text-secondary container shadow shadow-lg rounded px-4 py-5 text-center mt-5">
+        <div className="py-4">
+          <h1 className="display-5 fw-bold text-dark">Meeting Name</h1>
+          <p className="fs-5 mb-4 pe-5 d-flex justify-content-end">
+            Date: 27/05/2023
+          </p>
+          <h4 className="ps-5">
+            "Meeting Desc": Lorem ipsum dolor sit amet consectetur, adipisicing
+            elit. Minima, itaque? Consequatur ad quasi ipsam exercitationem fuga
+            facilis harum autem cupiditate! Architecto earum soluta ducimus
+            dolore quam, aliquid veritatis possimus facere.
+          </h4>
+          <hr />
+          <div className="col-lg-6 mx-auto">
+            <p className="fs-5 mb-4">
+              "Meeting Summary: "Quickly design and customize responsive
+              mobile-first sites with Bootstrap, the world’s most popular
+              front-end open source toolkit, featuring Sass variables and
+              mixins, responsive grid system, extensive prebuilt components, and
+              powerful JavaScript plugins.
+            </p>
+            <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+              <button
+                type="button"
+                className="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold"
+                onClick={editMeeting}
+              >
+                Edit
+              </button>
+              <button
+                type="button"
+                className="btn btn-outline-danger btn-lg px-4 "
+                onClick={handleDelete}
+              >
+                Delete
+              </button>
             </div>
-
+          </div>
+          <div>
+            {console.log(visible)}
+            {visible ? (
+              <div className="p-3">
+                <div className="input-group">
+                  <span className="input-group-text">Summary</span>
+                  <textarea
+                    className="form-control focus-ring focus-ring-light"
+                    aria-label="With textarea"
+                    defaultValue="Original meeting summary"
+                    onChange={(e) => setMsummary(e.target.value)}
+                  ></textarea>
+                </div>
+                <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+                  <button
+                    type="button"
+                    className="btn btn-sm btn-outline-success btn-lg px-4 mt-3 "
+                    onClick={saveDetails}
+                  >
+                    Save
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-sm btn-outline-danger btn-lg px-4 mt-3"
+                    onClick={() => setVisible(false)}
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </div>
+            ) : null}
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
-
