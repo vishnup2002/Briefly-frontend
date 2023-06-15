5709369dea6d0413f5d06f269604009c3da9485a
Vishnu P <vishnupothia@gmail.com>
2023-06-15 11:13:00 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,63 +1,58 @@
-import './App.css'
-import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom"
-import {useEffect} from 'react';
-import React, {useState} from 'react';
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import Create from './pages/Create'
-import Dashboard from './pages/Dashboard'
-import About from './pages/About'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Details from './pages/Details'
+import "./App.css";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { useEffect } from "react";
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Create from "./pages/Create";
+import Dashboard from "./pages/Dashboard";
+import About from "./pages/About";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Details from "./pages/Details";
 
 function App() {
+  const [name, setName] = useState("");
 
-  const [name, setName] = useState('');
+  console.log(name);
 
-  console.log(name)
+  //   useEffect(() => {
+  //     (
+  //         async () => {
+  //             const response = await fetch('http://localhost:8000/user/user', {
+  //                 headers: {'Content-Type': 'application/json'},
+  //                 credentials: 'include',
+  //             });
 
-  useEffect(() => {
-    (
-        async () => {
-            const response = await fetch('http://localhost:8000/user/user', {
-                headers: {'Content-Type': 'application/json'},
-                credentials: 'include',
-            });
+  //             const content = await response.json();
 
-            const content = await response.json();
+  //             if (content.hasOwnProperty("name")) {
+  //               setName(content.name);
+  //             }
 
-            if (content.hasOwnProperty("name")) {
-              setName(content.name);
-            }
-            
-        }
-    )();
-});
+  //         }
+  //     )();
+  // });
 
   return (
     <div>
       <BrowserRouter>
-        <Navbar name={name} setName={setName}/>
+        <Navbar name={name} setName={setName} />
         <Routes>
-          <Route path="/" element = {<Home name={name}/>}/>
-          <Route path="/about" element = {<About/>}/>
-          <Route path="/login" element = {<Login setName={setName}/>}/>
-          <Route path="/register" element = {<Register/>}/>
-          {name.length ? (
-          <>
-            <Route path="/dashboard" element = {<Dashboard name={name}/>}/>
-            <Route path="/create" element = {<Create name={name}/>}/>
-            <Route path="/meetings/:id" element={<Details name={name}/>}/>
-          </>
-          ) : (
-            <Route path="*" element={<Navigate to="/" />} />
-          )}
-      </Routes>
-    </BrowserRouter>
-    </div>
+          <Route path="/" element={<Home name={name} />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login setName={setName} />} />
+          <Route path="/register" element={<Register />} />
 
+          <Route path="/dashboard" element={<Dashboard name={name} />} />
+          <Route path="/create" element={<Create name={name} />} />
+          <Route path="/meetings/:id" element={<Details name={name} />} />
+
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
-} 
+}
 
 export default App;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,71 +1,111 @@
-import React from 'react'
-import { useState } from 'react'
-import {useNavigate} from 'react-router-dom';
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Register() {
-
   const navigate = useNavigate();
 
-  const [name,setName] = useState('')
-  const [email,setEmail] = useState('')
-  const [password,setPassword] = useState('')
-  const [redirect,setRedirect] = useState(false)
-  const [errorMessage, setErrorMessage] = useState(null)
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmpassword, setConfirmPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   const submit = async (e) => {
     e.preventDefault();
-    console.log({name,email,password})
+    console.log({ name, email, password });
 
-    const response = await fetch('http://localhost:8000/user/register', {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                name,
-                email,
-                password
-            })
-        });
+    if (password !== confirmpassword) {
+      setErrorMessage("Passwords do not match");
+      return;
+    }
 
-      const content = await response.json();
-      console.log(content);
-      if(content.hasOwnProperty('error')){
-        setErrorMessage(content.error);
-      }
-      else{
-        setRedirect(true)
-      }    
-  }
+    const response = await fetch("http://localhost:8000/user/register", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+      }),
+    });
 
-  if(redirect){
-    navigate('/login');
+    const content = await response.json();
+    console.log(content);
+    if (content.hasOwnProperty("error")) {
+      setErrorMessage(content.error);
+    } else {
+      setRedirect(true);
+    }
+  };
+
+  if (redirect) {
+    navigate("/login");
   }
 
   return (
     <div className="d-flex justify-content-center pt-5  mt-5">
-      <div className='border p-5 border-2 bg-light rounded'>
-        <h5 className="display-6 fw-bold text-body-emphasis  text-center">Signup</h5>
-        <form onSubmit={(e)=>submit(e)}>
+      <div className="border p-5 border-2 bg-light rounded">
+        <h5 className="display-6 fw-bold text-body-emphasis  text-center">
+          Signup
+        </h5>
+        <form onSubmit={(e) => submit(e)}>
           <div className="form-group">
             <label htmlFor="inputUsename">Username</label>
-            <input type="username" className="form-control" id="inputUsername" placeholder="Username" onChange={e => setName(e.target.value)}></input>
+            <input
+              type="username"
+              className="form-control"
+              id="inputUsername"
+              placeholder="Username"
+              onChange={(e) => setName(e.target.value)}
+            ></input>
           </div>
           <div className="form-group">
             <label htmlFor="inputEmail">Email address</label>
-            <input type="email" className="form-control" id="inputEmail" placeholder="Enter email" onChange={e => setEmail(e.target.value)}></input>
+            <input
+              type="email"
+              className="form-control"
+              id="inputEmail"
+              placeholder="Enter email"
+              onChange={(e) => setEmail(e.target.value)}
+            ></input>
           </div>
           <div className="form-group">
             <label htmlFor="inputPassword">Password</label>
-            <input type="password" className="form-control" id="inputPassword" placeholder="Password" onChange={e => setPassword(e.target.value)}></input>
+            <input
+              type="password"
+              className="form-control"
+              id="inputPassword"
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            ></input>
           </div>
-          
+          <div className="form-group">
+            <label htmlFor="inputConfirmPassword">Confirm Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="inputConfirmPassword"
+              placeholder="Password"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            ></input>
+          </div>
+
           <div className="d-flex flex-wrap justify-content-evenly pt-3">
             <div>
-              <button type="submit" className="btn btn-primary ">Sign in</button>
+              <button type="submit" className="btn btn-primary ">
+                Sign up
+              </button>
             </div>
           </div>
-          {errorMessage && <div className="alert alert-danger text-center mt-3" role="alert">{errorMessage}</div>}  
+          {errorMessage && (
+            <div className="alert alert-danger text-center mt-3" role="alert">
+              {errorMessage}
+            </div>
+          )}
         </form>
       </div>
     </div>
-  )
+  );
 }
